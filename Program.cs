cccf6ef93a59ae5f6ae5b0d975b09dcb930564b2
grandjean <mon-mail@mail.com>
2024-03-18 10:13:42 +0100

    Version 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int i;
 Console.WriteLine("Saisir la valeur");
-i =Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(i*i);
+i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Le carré de {i} est : {i*i}");
